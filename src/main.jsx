bf5d802219734b899267e9391933e608731cea2c
Nacho <nachomoisa39@gmail.com>
2024-05-23 16:44:03 -0300

    Un buscador de pelis basico para practicar el uso del fetch

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BuscadorPeliculas } from './BuscadorPeliculas.jsx'
+import './styles/Buscador.css'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BuscadorPeliculas />
+  </React.StrictMode>,
+)
